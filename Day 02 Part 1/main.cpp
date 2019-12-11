4d230c6d80723650bd1f4bc2c20ae1b76b73b1fb
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-11 23:12:43 +0100

    Some refactoring and renaming ...
    ... most important change: the map to functions has been replaced with a generalized lambda and a switch. Easier to read.

--- Day 02 Part 1/main.cpp
+++ Day 02 Part 1/main.cpp
@@ -1,12 +1,11 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
-#include "unordered_map.hpp"
+#include <unordered_map>
 
 struct Position {
-	std::intmax_t x = 0;
-	std::intmax_t y = 0;
+	std::int64_t x = 0;
+	std::int64_t y = 0;
 };
 
 auto operator==(const Position& lhs, const Position& rhs) {
@@ -20,66 +19,58 @@ namespace std {
 	template <>
 	struct hash<Position>{
 		auto operator()(const Position& pos) const {
-			return std::bit_xor{}(get_hash(pos.x), get_hash(pos.y));
+			return (get_hash(pos.x) ^ get_hash(pos.y));
 		}
 	};
 }
 
-constexpr auto MIN = -1L;
-constexpr auto MAX = 1L;
-
-auto up(Position& pos) {
-	if(pos.y + 1 <= MAX) { ++pos.y; }
-}
-
-auto down(Position& pos) {
-	if(pos.y - 1 >= MIN) { --pos.y; }
-}
-
-auto left(Position& pos) {
-	if(pos.x - 1 >= MIN) { --pos.x; }
-}
-
-auto right(Position& pos) {
-	if(pos.x + 1 <= MAX) { ++pos.x; }
-}
-
 int main() {
 
-	auto filename = std::string{"instructions.txt"};
+	const auto filename = std::string{"instructions.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
 
-		using cmd = void (*) (Position&);
-		auto cmd_map = ska::flat_hash_map<char, cmd>{
-			{'U', up},
-			{'D', down},
-			{'L', left},
-			{'R', right}
+		const auto move_to_key = [] (auto condition, auto& dimension, auto modifier) {
+			if(condition) {
+				dimension += modifier;
+			}
 		};
 
-		auto key_map = ska::flat_hash_map<Position, char> {
-			{{-1,1}, '1'},
-			{{0,1}, '2'},
-			{{1,1}, '3'},
-			{{-1,0}, '4'},
-			{{0,0}, '5'},
-			{{1,0}, '6'},
-			{{-1,-1}, '7'},
-			{{0,-1}, '8'},
-			{{1,-1}, '9'}
+		// we treat the keypad as a coordinate system
+		const auto keypad = std::unordered_map<Position, char> {
+			{{-1, 1}, '1'}, {{0, 1}, '2'}, {{1, 1}, '3'},
+			{{-1, 0}, '4'}, {{0, 0}, '5'}, {{1, 0}, '6'},
+			{{-1,-1}, '7'}, {{0,-1}, '8'}, {{1,-1}, '9'}
 		};
 
 		auto code = std::string{};
-		auto position = Position{};
+
+		auto pos = Position{};
+		auto& x = pos.x;
+		auto& y = pos.y;
+
+		// no coordinate can go beyond 1 in
+		// either positive or negative direction
+		constexpr auto MIN = std::int64_t{-1};
+		constexpr auto MAX = std::int64_t{1};
 
 		std::string line;
+
 		while(file >> line) {
 
-			for(const auto step : line) { cmd_map[step](position); }
+			for(const auto move : line) {
 
-			code.push_back(key_map[position]);
+				switch(move) {
+					case 'U': move_to_key((y < MAX), y,  1); break;
+					case 'D': move_to_key((y > MIN), y, -1); break;
+					case 'L': move_to_key((x > MIN), x, -1); break;
+					case 'R': move_to_key((x < MAX), x,  1); break;
+					default: std::cout << "WTF?!?" << std::endl;
+				}
+			}
+
+			code.push_back(keypad.find(pos)->second);
 		}
 
 		std::cout << code << std::endl;
