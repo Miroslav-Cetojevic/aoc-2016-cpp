b6387aa23afa45c8731e12ae66bf8edebc445010
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-13 17:14:29 +0100

    Uploaded the correct file now

--- Day 05 Part 1/main.cpp
+++ Day 05 Part 1/main.cpp
@@ -1,118 +1,58 @@
+/*
+ * NOTE:
+ *
+ * I'm using the following flags:
+ *
+ * Linker: -lPocoFoundation
+ *
+ */
 #include <algorithm>
-#include <fstream>
 #include <iostream>
 #include <string>
-#include <unordered_map>
-#include <vector>
 
-void my_split(std::vector<std::string>& tokens, const std::string& str, const std::string& delims = " ") {
+#include <Poco/DigestEngine.h>
+#include <Poco/DigestStream.h>
+#include <Poco/MD5Engine.h>
 
-	tokens.clear();
-
-    decltype(tokens.size()) current, previous = 0;
-
-    current = str.find_first_of(delims);
-
-    while(current != std::string::npos) {
-
-        tokens.push_back(str.substr(previous, current - previous));
-
-        previous = (current + 1);
-
-        current = str.find_first_of(delims, previous);
-    }
-
-    // there's still some string left
-    if(previous < str.size()) {
-    	tokens.push_back(str.substr(previous));
-    }
-}
-
+// NOTE: it takes a couple seconds for this program to finish
 int main() {
 
-	const auto filename = std::string{"rooms.txt"};
-	auto file = std::fstream{filename};
+	auto pwd = std::string{};
 
-	if(file.is_open()) {
+	constexpr decltype(pwd.size()) pwdlen = 8;
 
-		using Count = std::uint64_t;
+	const auto door_id = std::string{"reyedfim"};
 
-		auto letter_counts = std::unordered_map<char, Count>{};
+	const auto prefix = std::string{"00000"};
+	const auto prefix_size = prefix.size();
 
-		auto tokens = std::vector<std::string>{};
+	auto md5 = Poco::MD5Engine{};
+	auto out = Poco::DigestOutputStream{md5};
 
-		std::string line;
+	for(decltype(pwd.size()) i = 0; (pwd.size() < pwdlen); ++i) {
 
-		auto found_room = false;
+		// interestingly enough, if I write out << (door_id + std::to_string(i))
+		// instead, the program becomes slower - I guess, the additional time is
+		// spent on creating strings over and over again
+		out << door_id << i;
 
-		while(!found_room && std::getline(file, line)) {
+		out.close();
 
-			my_split(tokens, line, "-[]");
+		// retrieve the hash in hexadecimal form as a string
+		const auto result = Poco::DigestEngine::digestToHex(md5.digest());
 
-			const auto new_end = tokens.end() - 2;
+		// uncomment this if you want to watch the program generating the password
+//		std::cout << pwd << result[prefix_size] << std::endl;
 
-			// sector id is always the second to last token
-			const auto sector_id = std::stoul(*new_end);
+		const auto has_prefix = (result.compare(0, prefix_size, prefix) == 0);
 
-			// checksum is always the last token
-			const auto checksum = tokens.back();
-
-			tokens.erase(new_end);
-
-			letter_counts.clear();
-			for(const auto& token : tokens) {
-
-				for(const auto letter : token) {
-					++letter_counts[letter];
-				}
-			}
-
-			const auto checksum_begin = checksum.begin();
-			const auto checksum_end   = checksum.end();
-
-			const auto comparator = [&letter_counts] (const auto first_letter, const auto second_letter) {
-
-				const auto first_letter_count = letter_counts[first_letter];
-				const auto second_letter_count = letter_counts[second_letter];
-
-				const auto letters_exist = ((first_letter_count > 0) && (second_letter_count > 0));
-				const auto ordered_by_frequency = (first_letter_count > second_letter_count);
-				const auto tied_by_frequency = (first_letter_count == second_letter_count);
-				const auto ordered_by_alphabet = (first_letter < second_letter);
-
-				return !(letters_exist && (ordered_by_frequency || (tied_by_frequency && ordered_by_alphabet)));
-			};
-
-			const auto validator = std::adjacent_find(checksum_begin, checksum_end, comparator);
-
-			// we need to go through the whole checksum before we can conclude that it's valid,
-			// so the only way it COULD be valid is if the validator is at the end of it
-			const auto is_checksum_valid = (validator == checksum_end);
-
-			if(is_checksum_valid) {
-
-				constexpr auto a = 'a';
-				constexpr auto mod = 26;
-
-				for(auto& token : tokens) {
-
-					for(auto& letter : token) {
-						// behold, the state-of-the-art shift cipher
-						letter = (((letter - a + sector_id) % mod) + a);
-					}
-
-					if(token.find("north") != token.npos) {
-						std::cout << sector_id << std::endl;
-						found_room = true;
-						break;
-					}
-				}
-			}
+		// this is where the password construction happens
+		if(has_prefix) {
+			pwd += result[prefix_size];
 		}
-
-	} else {
-		std::cerr << "Error! Could not open file \"" << filename << "\"!" << std::endl;
 	}
+
+	std::cout << "Password: " << pwd << std::endl;
 
 	return 0;
 }
