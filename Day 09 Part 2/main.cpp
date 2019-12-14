168d83b44fc5c873af1032f1affc871010d4d500
Miroslav Cetojevic <miroslav.cetojevic@gmail.com>
2019-12-14 19:44:15 +0100

    Const (almost) all the things!

--- Day 09 Part 2/main.cpp
+++ Day 09 Part 2/main.cpp
@@ -1,47 +1,57 @@
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <string>
 
-template<typename S, typename N = std::uintmax_t>
+template<typename S, typename N = std::uint64_t>
 auto decompress(const S& compressed, N repeat = 1) -> decltype(auto) {
 
-	static constexpr auto marker_on = '(';
+	const auto compressed_size = compressed.size();
+
+	static constexpr auto marker_on  = '(';
 	static constexpr auto marker_off = ')';
 	static constexpr auto multiplier = 'x';
 
-	static auto get_next = [] (auto number) { return ++number; };
+	static constexpr auto get_next = [] (auto number) {
+		return ++number;
+	};
 
-	static auto get_value = [] (const auto& seq, auto start, auto end) {
+	static constexpr auto get_value = [] (const auto& seq, auto start, auto end) {
 		return std::stoul(seq.substr(start, (end - start)));
 	};
 
-	static auto end_of_string = [&] (auto pos) { return (pos < compressed.size()); };
+	const auto end_of_string = [compressed_size] (auto pos) {
+		return (pos < compressed_size);
+	};
 
 	auto marker_begin = compressed.find(marker_on);
 
 	auto has_marker = end_of_string(marker_begin);
 
-	if(!has_marker) { return (repeat * compressed.size()); }
+	// ends the recursion for the current compressed substring
+	if(!has_marker) {
+		return (repeat * compressed_size);
+	}
 
 	// if there are any letters before the first marker, they are counted here
 	auto accumulator = marker_begin;
 
 	while(has_marker) {
 
-		auto sublength_begin = (marker_begin + 1);
-		auto sublength_end = compressed.find(multiplier, sublength_begin);
+		const auto sublength_begin = get_next(marker_begin);
+		const auto sublength_end   = compressed.find(multiplier, sublength_begin);
 
-		auto sublength = get_value(compressed, sublength_begin, sublength_end);
+		const auto sublength = get_value(compressed, sublength_begin, sublength_end);
 
-		auto subrepeat_begin = get_next(sublength_end);
-		auto subrepeat_end = compressed.find(marker_off, subrepeat_begin);
+		const auto subrepeat_begin = get_next(sublength_end);
+		const auto subrepeat_end   = compressed.find(marker_off, subrepeat_begin);
 
-		auto subrepeat = (repeat * get_value(compressed, subrepeat_begin, subrepeat_end));
+		const auto subrepeat = (repeat * get_value(compressed, subrepeat_begin, subrepeat_end));
 
-		auto substr_begin = get_next(subrepeat_end);
-		auto substr_end = (substr_begin + sublength);
+		const auto substr_begin = get_next(subrepeat_end);
+		const auto substr_end   = (substr_begin + sublength);
 
-		auto substring = compressed.substr(substr_begin, sublength);
+		const auto substring = compressed.substr(substr_begin, sublength);
 
 		accumulator += decompress(substring, subrepeat);
 
@@ -56,7 +66,7 @@ auto decompress(const S& compressed, N repeat = 1) -> decltype(auto) {
 
 		} else if(end_of_string(substr_end)) {
 
-			accumulator += (compressed.size() - substr_end);
+			accumulator += (compressed_size - substr_end);
 		}
 	}
 
@@ -65,16 +75,15 @@ auto decompress(const S& compressed, N repeat = 1) -> decltype(auto) {
 
 int main() {
 
-	auto filename = std::string{"compressed.txt"};
+	const auto filename = std::string{"compressed.txt"};
 	auto file = std::fstream{filename};
 
 	if(file.is_open()) {
 
-		auto compressed = std::string{};
-
-		file >> compressed;
+		const auto compressed = *(std::istream_iterator<std::string>{file});
 
 		std::cout << decompress(compressed) << std::endl;
+
 	} else {
 		std::cerr << "Error! Could not open file \"" << filename << "\"!" << std::endl;
 	}
